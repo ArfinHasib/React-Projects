4ec7264fd28b83dc7cef9462ff6aecdb890d2344
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-13 11:14:57 +0600

    08-Slide-to-unlock (creating the lock screen)

--- src/projects/08-slide-to-unlock/SlideToUnlock.jsx
+++ src/projects/08-slide-to-unlock/SlideToUnlock.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LockSlider from './LockSlider';
+import { AiFillUnlock } from 'react-icons/ai';
 
 export default function SlideToUnlock() {
+   const [showLockSlider, setShowLockSlider] = useState(false);
+
    return (
-      <div>
-         <LockSlider />
+      <div
+         className='container text-center d-flex flex-column border-20'
+         style={{
+            height: '70vh',
+            marginTop: '15vh',
+            width: 340,
+            border: '4px solid #121212',
+         }}
+      >
+         <h1 className='title'>title</h1>
+         {showLockSlider ? (
+            <LockSlider width='250px' />
+         ) : (
+            <AiFillUnlock className='unlockIcon' />
+         )}
       </div>
    );
 }
