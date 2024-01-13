558e484441fcebc4f1e9277d70415f490699d478
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-13 10:40:14 +0600

    08-slide-to-unlock (building the UI)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import LightDarkMode from './projects/07-Light&DarkMode/Light&DarkMode';
 // import LikeMyPhoto from './projects/0.-like-my-photo/LikeMyPhoto';
 // import RandomizeColor from './projects/02-randomize-color/RandomizeColor';
 // import EsignatureApp from './projects/01-e-signature/EsignatureApp';
 // import Testimonials from './projects/04-testimonials-app/Testimonials';
 // import AlertApp from './projects/05-Alerts/AlertApp';
 // import TempControllerApp from './projects/06-Temp-Controller/TempControllerApp';
+// import LightDarkMode from './projects/07-Light&DarkMode/Light&DarkMode';
+import SlideToUnlock from './projects/08-slide-to-unlock/SlideToUnlock';
 
 function App() {
    return (
@@ -16,7 +17,8 @@ function App() {
          {/* <Testimonials /> */}
          {/* <AlertApp /> */}
          {/* <TempControllerApp /> */}
-         <LightDarkMode />
+         {/* <LightDarkMode /> */}
+         <SlideToUnlock />
       </div>
    );
 }
---
+++ src/projects/08-slide-to-unlock/SlideToUnlock.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LockSlider from './LockSlider';
+
+export default function SlideToUnlock() {
+   return (
+      <div>
+         <LockSlider />
+      </div>
+   );
+}
