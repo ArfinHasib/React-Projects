a622bbf6c801efbb7b9a0c2b25c0bda2a7f136ce
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-16 21:02:27 +0600

    12-ProgressBarContainer(Ui design an dinput auto focus with useRef and useEffect)

--- src/projects/12-Progress-Bar/ProgressBarContainer.jsx
+++ src/projects/12-Progress-Bar/ProgressBarContainer.jsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Title from '../components/Title';
 
 export default function ProgressBarContainer() {
-   return <div>ProgressBarContainer</div>;
+   const inputStyle = {
+      width: 50,
+      border: 'none',
+      textAlign: 'center',
+      borderBottom: '1px solide lightgray',
+   };
+
+   const uiInput = useRef(null);
+   useEffect(() => {
+      uiInput.current.focus();
+   });
+
+   return (
+      <div className='container container-sm m-auto text-center'>
+         <Title text='Progress bars' />
+         <h2>Project status:</h2>
+
+         <ul>
+            <li>
+               UI Status:{' '}
+               <input
+                  type='number'
+                  style={inputStyle}
+                  onChange={(e) => console.log(e.target.value)}
+                  ref={uiInput}
+                  value={''}
+               />
+            </li>
+            <li>
+               Data Status:{' '}
+               <input
+                  type='number'
+                  style={inputStyle}
+                  onChange={(e) => console.log(e.target.value)}
+                  value={''}
+               />
+            </li>
+         </ul>
+      </div>
+   );
 }
