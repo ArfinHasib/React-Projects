1f2804a16aaa31b4f74352674a27f5e47108a9a5
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-18 21:24:38 +0600

    13-DialogBoxApp(Hello Dialog Box Component)

--- src/projects/13-Dialog-box/DialogBoxApp.jsx
+++ src/projects/13-Dialog-box/DialogBoxApp.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
+import { BsFillEmojiFrownFill, BsFillEmojiSmileFill } from 'react-icons/bs';
 
 export default function DialogBoxApp({ width = 350 }) {
+   const [subscription, setSubscription] = useState({
+      title: 'Hello',
+      description: 'Would you like to subscribe?',
+      state: null,
+      justifyContent: 'space-between',
+   });
+
+   const handleSubscribe = () => {
+      setSubscription({
+         title: 'Thank You',
+         description: 'for your subscription',
+         state: 'subscribed',
+         justifyContent: 'flex-end',
+      });
+   };
+
+   const [icon, setIcon] = useState();
+   let iconStyle = {};
+
    return (
       <div className='card bg-light m-auto mt-4' style={{ width }}>
          <div className='card-body'>
@@ -15,17 +35,35 @@ export default function DialogBoxApp({ width = 350 }) {
                }}
             >
                <section>
-                  <h2 className='card-title'>title</h2>
-                  <h3 className='card-text'>text</h3>
+                  <h2 className='card-title'>{subscription.title}</h2>
+                  <h3 className='card-text'>{subscription.description}</h3>
                </section>
                <div>icon</div>
             </div>
          </div>
          <hr />
-         <div className='d-flex mb-2 px-1 text-end'>
-            <Button text={'Cancel'} btnClass={'btn-light'} />
-            <Button text={'Subscribe'} btnClass={'btn-danger'} />
-            <Button text={'Unsubscribe'} btnClass={'btn-danger btn-block'} />
+         <div
+            className='d-flex mb-2 px-1 text-end'
+            style={{
+               width: '100%',
+               justifyContent: `${subscription.justifyContent}`,
+            }}
+         >
+            {subscription.state === null && (
+               <Button text={'Cancel'} btnClass={'btn-light'} />
+            )}
+
+            {subscription.state === 'subscribed' ? null : (
+               <Button
+                  text={'Subscribe'}
+                  btnClass={'btn-danger'}
+                  onClick={handleSubscribe}
+               />
+            )}
+
+            {subscription.state === 'subscribed' ? (
+               <Button text={'Unsubscribe'} btnClass={'btn-danger btn-block'} />
+            ) : null}
          </div>
       </div>
    );
