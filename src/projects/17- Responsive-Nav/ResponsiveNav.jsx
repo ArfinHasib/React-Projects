6f7d2413e440c752eb0fbb7f56a2ed8569ff9f86
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-22 18:15:54 +0600

    17- Responsive Nav (Show and hide the navbar using React useState and useEffect)

--- src/projects/17- Responsive-Nav/ResponsiveNav.jsx
+++ src/projects/17- Responsive-Nav/ResponsiveNav.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavItem from './NavItem';
 import { NavStyle } from './NavStyle';
 
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
 export default function ResponsiveNav() {
+   const [showElement, setShowElement] = useState({
+      navBarNav: true,
+      hamburgerIcon: false,
+      closeIcon: false,
+      navOpen: false,
+   });
+
+   useEffect(() => {
+      window.onresize = () => {
+         let windowWidth = window.innerWidth;
+         windowWidth > 600 &&
+            setShowElement({ navBarNav: true, hamburgerIcon: false });
+         windowWidth < 600 &&
+            setShowElement({ navBarNav: false, hamburgerIcon: true });
+      };
+   });
+
    return (
       <NavStyle className='navbar bg-primary text-light'>
          <div className='navbar-logo'>
@@ -10,13 +29,17 @@ export default function ResponsiveNav() {
                Navbar BM
             </a>
             {/* Open and close icons */}
+            {showElement.hamburgerIcon && <AiOutlineMenu />}
+            {showElement.closeIcon && <AiOutlineClose />}
          </div>
          <div className='navbar-collapse'>
-            <ul className='navbar-items'>
-               <NavItem text='Home' active='active' />
-               <NavItem text='About' />
-               <NavItem text='Contact' />
-            </ul>
+            {showElement.navBarNav ? (
+               <ul className='navbar-items'>
+                  <NavItem text='Home' active='active' />
+                  <NavItem text='About' />
+                  <NavItem text='Contact' />
+               </ul>
+            ) : null}
          </div>
       </NavStyle>
    );
