f7bdf8cf0416d5ba8e699cf040422353761ab52b
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-23 21:44:25 +0600

    18 - Text Animation (Clear and Focus on Input Field)

--- src/projects/18- text-animation/TextAnimation.jsx
+++ src/projects/18- text-animation/TextAnimation.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Button from '../components/Button';
 import FormGroup from '../components/FormGroup';
+import AnimatedText from 'react-animated-text-content';
+export default function TextAnimationApp() {
+   const [inputValue, setInputValue] = useState('');
 
-export default function TextAnimation() {
+   const [animatedText, setAnimatedText] = useState('');
+   const handleInputChange = (e) => {
+      setInputValue(e.target.value);
+   };
+   const handleClear = () => {
+      setAnimatedText(inputValue);
+      setInputValue(() => '');
+   };
+
+   let inputText = useRef(null);
+   useEffect(() => {
+      inputText.current.focus();
+   });
    return (
       <div className='container text-center'>
          <form
@@ -12,12 +27,35 @@ export default function TextAnimation() {
             <FormGroup
                labelText={'Type in your text to be animated'}
                inputType='text'
-               placeholder='Your Text'
-               values=''
+               placeholder={'Your text'}
+               values={inputValue}
+               onChange={handleInputChange}
+               reference={inputText}
             />
-            <Button text='Clear' btnClass='btn-large btn-danger' />
+            <Button
+               text='Clear'
+               btnClass={'btn-large btn-danger'}
+               onClick={handleClear}
+            />
          </form>
-         animate Text
+
+         {animatedText && (
+            <AnimatedText
+               className='title'
+               // type="words"
+
+               type='char'
+               interval={0.04}
+               duration={1.1}
+               animation={{
+                  // x: '-150px',
+                  y: '-250px',
+                  ease: 'linear',
+               }}
+            >
+               {animatedText}
+            </AnimatedText>
+         )}
       </div>
    );
 }
