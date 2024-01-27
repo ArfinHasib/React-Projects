021f651b4ea91240541e0aea2541c0ae2b425f21
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-27 11:48:53 +0600

    20 - Space Penguin (Space Ship Component)

--- src/projects/20-Space-Penguin/SpacePenguinApp.jsx
+++ src/projects/20-Space-Penguin/SpacePenguinApp.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './ship.css';
 import Button from '../components/Button';
+import SpaceShipObj from './objects/SpaceShipObj';
+import Ship from './svgs/space-ship-rocket-svgrepo-com.svg';
 
 export default function SpacePenguinApp() {
    return (
       <div className='space'>
          <Button text='Launch' btnClass='btn-danger' onClick={''} />
          <section className='env'>
-            {/* <SpaceShip /> */}
+            <SpaceShipObj changeShip={Ship} scale={200} />
             {/* <MoonObj /> */}
             {/* <PlanetObj /> */}
          </section>
