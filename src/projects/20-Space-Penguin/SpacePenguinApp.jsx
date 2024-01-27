819274b9115c010f668306fa2fb91e80c9211fb9
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-27 11:55:10 +0600

    20 - Space Penguin (Earth and Moon Components)

--- src/projects/20-Space-Penguin/SpacePenguinApp.jsx
+++ src/projects/20-Space-Penguin/SpacePenguinApp.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './ship.css';
 import Button from '../components/Button';
 import SpaceShipObj from './objects/SpaceShipObj';
+import PlanetObj from './objects/PlanetObj';
+import MoonObj from './objects/MoonObj';
+
 import Ship from './svgs/space-ship-rocket-svgrepo-com.svg';
+import Earth from './svgs/planet-earth-svgrepo-com.svg';
+import Moon from './svgs/moon-svgrepo-com.svg';
 
 export default function SpacePenguinApp() {
    return (
@@ -10,8 +15,8 @@ export default function SpacePenguinApp() {
          <Button text='Launch' btnClass='btn-danger' onClick={''} />
          <section className='env'>
             <SpaceShipObj changeShip={Ship} scale={200} />
-            {/* <MoonObj /> */}
-            {/* <PlanetObj /> */}
+            <MoonObj Moon={Moon} />
+            <PlanetObj Planet={Earth} />
          </section>
          <Button text='Land' btnClass='btn-info' onClick={''} />
       </div>
---
+++ src/projects/20-Space-Penguin/objects/MoonObj.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function MoonObj({ Moon }) {
+   return (
+      <img
+         src={Moon}
+         alt='Moon Img'
+         className='moon
+   '
+      />
+   );
+}
