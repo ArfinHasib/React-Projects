debb9cf31efc008cfc821505b429e267195b499d
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-27 12:08:27 +0600

    20 - Space Penguin (Launch and Land the Ship)

--- src/projects/20-Space-Penguin/SpacePenguinApp.jsx
+++ src/projects/20-Space-Penguin/SpacePenguinApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ship.css';
 import Button from '../components/Button';
 import SpaceShipObj from './objects/SpaceShipObj';
@@ -10,15 +10,39 @@ import Earth from './svgs/planet-earth-svgrepo-com.svg';
 import Moon from './svgs/moon-svgrepo-com.svg';
 
 export default function SpacePenguinApp() {
+   const [shipAction, setShipAction] = useState();
+   const [transformScale, setTransformScale] = useState(200);
+
+   const [shipProps, setShipProps] = useState({
+      action: '',
+      scale: 200,
+   });
+
+   const startShip = () => {
+      // setShipAction('fly');
+      // setTransformScale(50)
+      setShipProps({ action: 'fly', scale: 50 });
+   };
+
+   const landShip = () => {
+      // setShipAction('land');
+      // setTransformScale(200)
+      setShipProps({ action: 'land', scale: 200 });
+   };
+
    return (
       <div className='space'>
-         <Button text='Launch' btnClass='btn-danger' onClick={''} />
+         <Button text='Launch' btnClass='btn-danger' onClick={startShip} />
          <section className='env'>
-            <SpaceShipObj changeShip={Ship} scale={200} />
+            <SpaceShipObj
+               changeShip={Ship}
+               scale={shipProps.scale}
+               start={shipProps.action}
+            />
             <MoonObj Moon={Moon} />
             <PlanetObj Planet={Earth} />
          </section>
-         <Button text='Land' btnClass='btn-info' onClick={''} />
+         <Button text='Land' btnClass='btn-info' onClick={landShip} />
       </div>
    );
 }
