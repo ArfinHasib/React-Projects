66c97c45afe520a87a9cb6ba3d7b15e79e7a4100
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-28 20:38:54 +0600

    21 - Task Tracker (Input Task Component)

---
+++ src/projects/21 - task-tracker/Task.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from '../components/Button';
+
+export default function Task({ date, type, setDate, setType, onClick }) {
+   return (
+      <div
+         className='d-flex'
+         style={{
+            justifyContent: 'space-between',
+            borderBottom: '1px solid #666',
+         }}
+      >
+         <input type='datetime-local' value={date} onChange={setDate} />
+         <input type='text' value={type} onChange={setType} />
+         <Button text='+ Add' btnClass='btn' onClick={onClick} />
+      </div>
+   );
+}
--- src/projects/21 - task-tracker/TaskTrackerApp.jsx
+++ src/projects/21 - task-tracker/TaskTrackerApp.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../components/Title';
 import { TableStyle } from './TableStyle';
+import Task from './Task';
 
 export default function TaskTrackerApp() {
+   const [newTask, setNewTask] = useState({
+      date: '',
+      type: '',
+      completed: '',
+   });
+
+   const handleChangeDate = (e) => {
+      setNewTask({ ...newTask, date: e.target.value });
+   };
+
+   const handleChangeType = (e) => {
+      setNewTask({ ...newTask, type: e.target.value });
+   };
+
    return (
       <>
          <Title text='Tasks Tracker' />
@@ -10,8 +25,13 @@ export default function TaskTrackerApp() {
             <ul className='table-haed'>
                <li>Date</li>
                <li>Task</li>
-               {/* <Task /> */}
             </ul>
+            <Task
+               date={newTask.date}
+               setDate={handleChangeDate}
+               type={newTask.type}
+               setType={handleChangeType}
+            />
             <ul className='table-row'>
                <li className='completed'>task</li>
                <li>task</li>
