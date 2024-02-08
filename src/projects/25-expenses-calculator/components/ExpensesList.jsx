e3a017804afc2b58362f3354dd67bc3e0e8bf87d
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-08 21:09:21 +0600

    25 - Expense Calc (Create a list Item)

--- src/projects/25-expenses-calculator/components/ExpensesList.jsx
+++ src/projects/25-expenses-calculator/components/ExpensesList.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { MdDelete } from 'react-icons/md';
+import ExpenseItem from './ExpenseItem';
 
 export default function ExpensesList({ clearItems, expenses = [] }) {
    return (
       <>
-         <ul className='list'>{/* <ExpenseItem /> */}</ul>
+         <ul className='list'>
+            <ExpenseItem />
+         </ul>
          {expenses.length > 0 && (
             <button className='btn btn-danger' onClick={clearItems}>
                <MdDelete /> Clear all expenses
