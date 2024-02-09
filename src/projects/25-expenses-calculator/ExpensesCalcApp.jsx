f6b0370f1d68a8b835d730d46c1087cfd8b5713f
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-09 17:26:35 +0600

    25 - Expense Calc (Add Alerts and finalize application)

--- src/projects/25-expenses-calculator/ExpensesCalcApp.jsx
+++ src/projects/25-expenses-calculator/ExpensesCalcApp.jsx
@@ -1,129 +1,147 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Title from '../components/Title';
 import ExpensesForm from './components/ExpensesForm';
 import ExpensesList from './components/ExpensesList';
+import Alert from './components/Alert';
 import { BudgetStyle } from './components/styles/Budget.style';
 import { v4 as uuidV4 } from 'uuid';
-
 const initialExpense = localStorage.getItem('expenses')
    ? JSON.parse(localStorage.getItem('expenses'))
    : [];
 
 export default function ExpensesCalcApp() {
+   // state values
    // All expenses
-   const [expenses, setExpenses] = useState(initialExpense);
-   // Single expense
+   const [expenses, setExpense] = useState(initialExpense);
+   // Single Expense
    const [date, setDate] = useState('');
    // Single Amount
    const [amount, setAmount] = useState('');
-   // Single charge
+   // Single Charge
    const [charge, setCharge] = useState('');
    // Budget
    const [budget, setBudget] = useState('');
-
-   // ids
+   // Id's
    const [id, setId] = useState(0);
    // Edit?
    const [edit, setEdit] = useState(false);
+   // Alert
+   const [alert, setAlert] = useState({ show: false });
+   // Handlers
 
+   // handle Budget
    const changeBudget = (e) => {
-      setBudget(e.target.value);
+      // setBudget(e.target.value);
+      setBudget(inputBudget.current.value);
    };
-
+   // handle Charge
    const handleCharge = (e) => {
       setCharge(e.target.value);
    };
-
+   // handle Date
    const handleDate = (e) => {
       setDate(e.target.value);
    };
-
+   // handle Amount
    const handleAmount = (e) => {
       setAmount(e.target.value);
    };
+   // handle Alert
+
+   const handleAlert = ({ type, text }) => {
+      setAlert({ show: true, type, text });
+      setTimeout(() => {
+         setAlert({ show: false });
+      }, 3000);
+   };
+   //   Handle Submit
 
    const handleSubmit = (e) => {
       e.preventDefault();
-
       if (date !== '' && charge !== '' && amount > 0) {
          if (edit) {
             let tempExpense = expenses.map((item) => {
                return item.id === id ? { ...item, date, charge, amount } : item;
             });
-            setExpenses(tempExpense);
+            setExpense(tempExpense);
             setEdit(false);
             // todo: Alert
+            handleAlert({ type: 'success', text: 'Expense Edited' });
          } else {
             const singleExpense = { id: uuidV4(), date, charge, amount };
-            setExpenses([...expenses, singleExpense]);
+            setExpense([...expenses, singleExpense]);
             // todo: Alert
+            handleAlert({ type: 'success', text: 'Expense added' });
          }
          setCharge('');
          setAmount('');
       } else {
-         // todo: alert
+         // todo: Alert
+         handleAlert({
+            type: 'danger',
+            text: 'Please complete all fields ',
+         });
       }
+      // Set Expense
    };
-
    let inputBudget = useRef(null);
    useEffect(() => {
       inputBudget.current.value === '' && inputBudget.current.focus();
-      inputBudget.current.focus();
       localStorage.setItem('expenses', JSON.stringify(expenses));
    }, [expenses]);
 
    // Handle Clear all Expense
-   const clearExpenses = () => {
-      setExpenses([]);
-      // Alert
-   };
+   const clearAllExpenses = () => {
+      setExpense([]);
+      // todo: Alert
 
-   // Handle delete single expense
+      handleAlert({ type: 'danger', text: 'All expense deleted' });
+   };
+   // Handler Delete one expense
+
    const handleDelete = (id) => {
-      if (window.confirm('Delete Expense?')) {
-         let filterdExpenses = expenses.filter((expense) => expense.id !== id);
-         setExpenses(filterdExpenses);
-         // todo: alert
+      if (window.confirm('Delete expense?')) {
+         let filteredExpense = expenses.filter((expense) => expense.id !== id);
+         setExpense(filteredExpense);
+         // todo: Alert
+         handleAlert({ type: 'danger', text: 'Expense deleted' });
       }
    };
 
-   // Handle Edit single expense
    const handleEdit = (id) => {
-      let editedExpenses = expenses.find((expense) => expense.id === id);
-      let { charge, amount } = editedExpenses;
+      let editedExpense = expenses.find((expense) => expense.id === id);
+      let { charge, amount } = editedExpense;
       setCharge(charge);
       setAmount(amount);
       setId(id);
       setEdit(true);
    };
-
    return (
       <main className='container'>
-         <Title text='Expenses Calculator' />
-         {/* Alert Component */}
+         <Title text={'Expenses Calculator'} />
+         {alert.show && <Alert type={alert.type} text={alert.text} />}
 
          <section
             style={{
                display: 'grid',
-               gridTemplateColumns: 'repeat(2, 1fr)',
-               gap: '25px',
+               gridTemplateColumns: 'repeat(2,1fr)',
+               gap: '25px ',
                margin: '1rem',
             }}
          >
             <aside>
-               {/* ExpensesForm */}
                <ExpensesForm
                   date={date}
-                  amount={amount}
-                  charge={charge}
                   handleDate={handleDate}
+                  charge={charge}
                   handleCharge={handleCharge}
+                  amount={amount}
                   handleAmount={handleAmount}
                   handleSubmit={handleSubmit}
                   edit={edit}
                />
 
-               <section className='card mt-2 bg-primary text-light'>
+               <section className='card mt-2 bg-primary text-light text-right'>
                   <div className='card-body'>
                      <BudgetStyle>
                         <h3>Budget : $</h3>
@@ -135,12 +153,12 @@ export default function ExpensesCalcApp() {
                         />
                      </BudgetStyle>
                      <h3 className='mb-1'>
-                        Total Expenses: ${' '}
+                        Total expenses: ${' '}
                         {expenses.reduce((total, expense) => {
                            return (total += parseInt(expense.amount, 10));
                         }, 0)}
                      </h3>
-                     {/* Calc Economies */}
+                     {/* Calc economies */}
                      <h3>Economies: $ {calcEconomies(budget, expenses)}</h3>
                   </div>
                </section>
@@ -150,14 +168,13 @@ export default function ExpensesCalcApp() {
                   expenses={expenses}
                   handleDelete={handleDelete}
                   handleEdit={handleEdit}
-                  handleClearAllExpenses={clearExpenses}
+                  handleClearAllExpenses={clearAllExpenses}
                />
             </section>
          </section>
       </main>
    );
 }
-
 function calcEconomies(budget, expenses) {
    return (
       budget -
