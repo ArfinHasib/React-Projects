599a233e28e7ffd39dc85d5ed0915b5df1cdaf57
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-19 11:15:53 +0600

    27 - Course Store (Convert the currencies with the react useContext hook)

--- src/projects/27- course-store/Store.jsx
+++ src/projects/27- course-store/Store.jsx
@@ -3,6 +3,7 @@ import Button from '../components/Button';
 import Courses from './components/Courses';
 import { coursesDB } from './db/coursesDB';
 import { currenciesDB } from './db/currenciesDB';
+import { CurrencyContext } from './context/currencies-context';
 
 document.body.style.backgroundColor = '#282c34';
 document.body.style.color = '#eee';
@@ -11,26 +12,28 @@ export default function Store() {
    const [currency, setCurrency] = useState(currenciesDB.Euro);
 
    return (
-      <div className='container p-1'>
-         <h4 className='mb-2'>Change Currency:</h4>
-         {Object.values(currenciesDB).map((curr) => (
-            <Button
-               btnClass='btn-light btn-sm'
-               key={curr.code}
-               text={curr.code}
-               onClick={() => setCurrency(curr)}
-            />
-         ))}
-         <header className='text-center my-4'>
-            <h1 className='title fs-xl'>Course</h1>
-            <h2 className='text-uppercase mb-2'>Become a web developer</h2>
-            <p className='mx-2'>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit.
-               Laboriosam harum ex, molestiae unde fugiat esse sit minus labore
-               nesciunt omnis!
-            </p>
-         </header>
-         <Courses list={coursesDB} />
-      </div>
+      <CurrencyContext.Provider value={currency}>
+         <div className='container p-1'>
+            <h4 className='mb-2'>Change Currency:</h4>
+            {Object.values(currenciesDB).map((curr) => (
+               <Button
+                  btnClass='btn-light btn-sm'
+                  key={curr.code}
+                  text={curr.code}
+                  onClick={() => setCurrency(curr)}
+               />
+            ))}
+            <header className='text-center my-4'>
+               <h1 className='title fs-xl'>Course</h1>
+               <h2 className='text-uppercase mb-2'>Become a web developer</h2>
+               <p className='mx-2'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laboriosam harum ex, molestiae unde fugiat esse sit minus
+                  labore nesciunt omnis!
+               </p>
+            </header>
+            <Courses list={coursesDB} />
+         </div>
+      </CurrencyContext.Provider>
    );
 }
--- src/projects/27- course-store/components/Course.jsx
+++ src/projects/27- course-store/components/Course.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Button from '../../components/Button';
+import { CurrencyContext } from '../context/currencies-context';
 
 export default function Course({ course }) {
    const { title, img, price } = course;
+
+   const currency = React.useContext(CurrencyContext);
+
+   const contextPrice = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: currency.code,
+   }).format(price * currency.conversionRate);
 
    return (
       <li className='card mb-2' style={{ width: 250 }}>
@@ -14,7 +22,7 @@ export default function Course({ course }) {
             Dignissimos, nemo!
          </p>
          <div className='card-footer d-flex space-between'>
-            <h4>{price}</h4>
+            <h4>{contextPrice}</h4>
             <Button btnClass='btn-success' text='BUY' />
          </div>
       </li>
